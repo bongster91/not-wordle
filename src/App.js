4bd0100cd69a1319d2a21fdb05f9557397a2ba5d
Grace Bong <gracebong.park@gmail.com>
2023-02-09 00:11:19 -0500

    react foundation added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      not wordle
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navBar.js
@@ -0,0 +1,7 @@
+function navBar() {
+  return (
+    <h1>Not Wordle</h1>
+  );
+};
+
+export default navBar;
