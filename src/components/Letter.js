7bd0e3709c34a7e734e2026a80fd52399e93893a
Grace Bong <gracebong.park@gmail.com>
2023-02-10 23:25:28 -0500

    minor refactor

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -2,21 +2,32 @@ import React, { useContext, useEffect } from 'react';
 import { AppContext } from '../App'; 
 
 function Letter({ letterPosition, attemptValue }) {
-    const { board, correctWord, currentAttempt, disabledLetters, setDisabledLetters } = useContext(AppContext);
+    const { board, correctWord, currentAttempt, disabledLetters, setDisabledLetters, pressEnter,  } = useContext(AppContext);
     const letter = board[attemptValue][letterPosition];
 
     const correct = correctWord[letterPosition] === letter;
     const almost =  !correct && letter !== '' && correctWord.split('').includes(letter);
-
-    const letterState = currentAttempt.attempt > attemptValue && 
+    
+    let letterState = currentAttempt.attempt > attemptValue && 
         (correct ? 'correct' : almost ? 'almost' : 'error')
+
+    if (currentAttempt.attempt > attemptValue && pressEnter) {
+        if (correct) {
+            letterState = 'correct'
+        } else if (almost) {
+            letterState = 'almost'
+        } else {
+            letterState = 'error'
+        };
+    };
 
     useEffect(() => {
         if (letter !== '' && !correct && !almost) {
             setDisabledLetters((prev) =>  disabledLetters.add(letter))
         }
+
     }, [almost, correct, disabledLetters, letter, setDisabledLetters]);
-    
+
     return (
         <div className='letter' id={letterState}>
             {letter}
